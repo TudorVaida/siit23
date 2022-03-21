3d90da3b6b2708ea17ec3a5aae6ca5cb5f402647
Tudor Vaida <vaida.tudor@gmail.com>
2022-03-21 21:39:32 +0200

    exercitii luni saptamaa 4

--- Saptamana4/script.js
+++ Saptamana4/script.js
@@ -20,4 +20,63 @@ function bolt(nr) {
 		} else console.log(i + " <--");
 	}
 }
-bolt(50);
+// bolt(50);
+
+const str = "Ana are mere!";
+function invers(str) {
+	let strInvers = "";
+	for (let i = str.length - 1; i >= 0; i--) {
+		//str[i] === str[str -1 -i]
+		strInvers += str[i];
+	}
+	return strInvers;
+}
+// console.log(invers(str));
+
+function impar(nr) {
+	for (let i = 1; i <= nr; i = i + 2) {
+		console.log(i);
+	}
+}
+// impar(20);
+const arr = [1, 2, 3, 4, 5];
+function suma(arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	return sum;
+}
+console.log(suma(arr));
+function max(arr) {
+	let max = arr[0];
+	for (let i = 0; i < arr.length; i++) {
+		if (max < arr[i]) {
+			max = arr[i];
+		}
+	}
+	return max;
+}
+console.log(max(arr));
+
+const arr2 = [1, 2, 4, 3, 5, 4, 3, 4, 5, 6, 4];
+function timesAppears(arr, a) {
+	let counter = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === a) {
+			counter++;
+		}
+	}
+	return counter;
+}
+console.log(timesAppears(arr2, 4));
+
+for (let i = 0; i < 4; i++) {
+	let row = "";
+	for (let y = 0; y < 4; y++) {
+		if ((i + y) % 2 === 0) {
+			row += "0 ";
+		} else row += "1 ";
+	}
+	console.log(row);
+}
