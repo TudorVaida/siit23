baf17ede049a32f6d9c954229f75905c7b1eda4f
Tudor Vaida <vaida.tudor@gmail.com>
2022-04-19 12:09:22 +0300

    loading

--- Saptamana8/script.js
+++ Saptamana8/script.js
@@ -4,8 +4,11 @@ let search = "";
 let url = "https://meniu-c6eb3-default-rtdb.europe-west1.firebasedatabase.app/";
 
 async function getData() {
+	document.querySelector("#loading").classList.remove("hidden");
 	const r = await fetch(url + ".json");
 	list = await r.json();
+	document.querySelector("#loading").classList.add("hidden");
+
 	console.log(list);
 	draw();
 }
